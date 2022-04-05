1f5f6ca79f0e02298297bc2367d265f260227808
otto <otto.o@cybertekschool.com>
2022-04-05 12:47:26 +0300

    anaother solution for algo4

--- src/main/java/algo4_array_of_products/Algo4_1.java
+++ src/main/java/algo4_array_of_products/Algo4_1.java
@@ -2,25 +2,25 @@ package main.java.algo4_array_of_products;
 
 import java.util.Arrays;
 
-public class Algo4 {
+public class Algo4_1 {
+
     public static void main(String[] args) {
 
         int[] array = {5, 1, 4, 2};
         System.out.println("Arrays.toString(arrayOfProducts(array)) = " + Arrays.toString(arrayOfProducts(array)));
+
     }
 
     public static int[] arrayOfProducts(int[] array){
 
-        int[] resultArray= new int[array.length];
+        int totalProducts = 1;
+        for (int i = 0; i < array.length; i++){
+            totalProducts *= array[i];
+        }
 
+        int[] resultArray= new int[array.length];
         for(int i=0; i<array.length; i++){
-            int products = 1;
-            for(int j=0; j<array.length; j++){
-                if(i!=j){
-                    products *= array[j];
-                }
-            }
-            resultArray[i] = products;
+            resultArray[i] = totalProducts / array[i];
         }
         return resultArray;
     }
