5521f6226b85f64c443eb80b6b7ebbdb98051bfd
otto <otto.o@cybertekschool.com>
2022-04-13 11:43:10 +0300

    solution for 0032_roman_to_integer

---
+++ src/main/java/_032_roman_to_integer/BruteForce.java
@@ -0,0 +1,83 @@
+package main.java._032_roman_to_integer;
+
+public class BruteForce {
+
+    public static void main(String[] args) {
+        int number = romanToInteger("MCD");
+        System.out.println("number = " + number);
+
+    }
+
+    public static int romanToInteger(String s){
+        int sum = 0;
+        boolean beforeI = false;
+        boolean beforeLC = false;
+        boolean beforeDM = false;
+        for (int i = 0; i < s.length(); i++){
+            switch (s.charAt(i)){
+                case 'V':
+                    if(beforeI){
+                        sum = sum + 3;
+                        beforeI = false;
+                    }else{
+                        sum += 5;
+                        beforeI = true;
+                    }
+                    break;
+                case 'L':
+                    if(beforeLC){
+                        sum = sum + 30;
+                        beforeLC = false;
+                    }else{
+                        sum += 50;
+                        beforeLC = true;
+                    }
+                    break;
+                case 'D':
+                    if(beforeDM){
+                        sum = sum + 300;
+                        beforeDM = false;
+                    }else{
+                        sum += 500;
+                        beforeDM = true;
+                    }
+                    break;
+                case 'M':
+                    if(beforeDM){
+                        sum = sum + 800;
+                        beforeDM = false;
+                    }else{
+                        sum += 1000;
+                        beforeDM = true;
+                    }
+                    break;
+                case 'I':
+                    sum += 1;
+                    beforeI = true;
+                    break;
+                case 'X':
+                    if(beforeI){
+                        sum = sum + 8;
+                        beforeI = false;
+                    }else{
+                        sum += 10;
+                        beforeLC = true;
+                    }
+                    break;
+                case 'C':
+                    if(beforeLC){
+                        sum = sum + 80;
+                        beforeLC = false;
+                    }else{
+                        sum += 100;
+                        beforeDM = true;
+                    }
+                    break;
+                default:
+                    System.out.println("There is no such Roman Literal");
+                    break;
+            }
+        }
+        return sum;
+    }
+}
