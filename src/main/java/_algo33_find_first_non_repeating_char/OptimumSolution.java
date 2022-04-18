430a98f0c05def76cf673b9a4a012f0b62c55d46
otto <otto.o@cybertekschool.com>
2022-04-18 13:14:41 +0300

    some changes for find first non repeating char

--- src/main/java/_algo33_find_first_non_repeating_char/OptimumSolution.java
+++ src/main/java/_algo33_find_first_non_repeating_char/OptimumSolution.java
@@ -9,26 +9,27 @@ public class OptimumSolution {
         System.out.println(findFirstNonRepeatingChar("a green apple"));
     }
 
-    //
+    // time complexity O(n), space complexity O(n)
     public static char findFirstNonRepeatingChar(String str){
         // create hashmap for char and its count
-        Map<Character,Integer> map=new HashMap<>();
-        int count=0;
+        Map<Character,Integer> map = new HashMap<>();
+        int count = 0;
+        // O(n)
         var chars=str.toCharArray();
         // O(n)
         for(Character ch: chars) {
             if (map.containsKey(ch)) {
-                count=map.get(ch);
-                map.put(ch, count+1);
+                count = map.get(ch);
+                map.put(ch, count + 1);
             }else {// first time we see ch
-                map.put(ch,1);
+                map.put(ch, 1);
             }
         }
         // O(n)
         for(Character ch: chars){
-            if (map.get(ch)==1) return ch;
+            if (map.get(ch) == 1) return ch;
         }
-        // O(n) + O(n) = O(2n) => O(n)
+        // O(n) + O(n) + O(n) = O(3n) => O(n) time complexity
         return Character.MIN_VALUE;
     }
 }
