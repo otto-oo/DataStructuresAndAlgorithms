81d4f2db0bb4b3c6f913544682716fa6760e066b
otto <otto.o@cybertekschool.com>
2022-08-03 10:38:46 +0300

    Days

---
+++ src/main/java/interview_examples/oracle/Days.java
@@ -0,0 +1,47 @@
+package main.java.interview_examples.oracle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+
+You have ("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun") for days.
+
+k is an integer
+
+You should implement given String day after k days.
+
+Return String of day..
+
+ */
+
+
+public class Days {
+    public static void main(String[] args) {
+        System.out.println("days(\"Wed\", 2) = " + days("Wed", 2));
+    }
+
+    public static String days(String day, int k){
+        int numberOfDay = daysMap().get(day);
+        k = (k + numberOfDay) % 7;
+        for (Map.Entry<String, Integer> entry: daysMap().entrySet()) {
+            if (entry.getValue().equals(k)){
+                day = entry.getKey();
+                break;
+            }
+        }
+        return day;
+    }
+
+    private static Map<String, Integer> daysMap(){
+        Map<String, Integer> daysMap = new HashMap<>();
+        daysMap.put("Mon", 0);
+        daysMap.put("Tue", 1);
+        daysMap.put("Wed", 2);
+        daysMap.put("Thu", 3);
+        daysMap.put("Fri", 4);
+        daysMap.put("Sat", 5);
+        daysMap.put("Sun", 6);
+        return daysMap;
+    }
+}
